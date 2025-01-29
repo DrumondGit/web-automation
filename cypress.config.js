e09763c199c2e68cada7d7bd6e9ce038d360d6c5
Guilherme Drumond Silva <drumond.work@gmail.com>
2025-01-29 13:38:42 -0300

    Adding configuration for desktop and mobile context

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,11 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "http://www.automationpractice.pl/index.php?controller=authentication&back=my-account",
+  },
+  env: {
+    mobileViewportWidthBreakpoint: 414,
+    mobile: "iphone-xr",
+    desktop: "macbook-15",
   },
 });
