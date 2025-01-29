9f4a0abbe41a6e2f6f7b9098cea85ec54758d99c
Guilherme Drumond Silva <drumond.work@gmail.com>
2025-01-29 15:12:39 -0300

    adding fixture and git pipeline

--- cypress/e2e/tests_regression/LoginTests.cy.js
+++ cypress/e2e/tests_regression/LoginTests.cy.js
@@ -1,105 +1,160 @@
 import LoginPage from "../pages/LoginPage";
 const loginPage = new LoginPage();
-const dataPage = require("../pages/DataPage");
-
-const email = dataPage.login.validEmail;
-const password = dataPage.login.validPassword;
-const invalidEmail = dataPage.login.invalidEmail;
-const invalidPassword = dataPage.login.invalidPassword;
-
 
 describe("Login", () => {
-    context("Desktop", () => {
+    beforeEach(function () {
+        cy.fixture("loginData").then((data) => {
+            this.data = data;
+        });
+    });
+
+    context("Desktop", function () {
         beforeEach(function () {
             cy.viewport(Cypress.env("desktop"));
             cy.visit("/");
         });
 
-        it('Test Case 1: Login - Verify if user can Log in with valid credentials"', () => {
-            loginPage.enterYourEmailField().type(email)
-            loginPage.enterYourPasswordField().type(password)
-            loginPage.submitLoginButton().click()
-            loginPage.validateLogin()
+        it('Test Case 1: Login - Verify if user can Log in with valid credentials"', function () {
+            /*
+                Given: User is on the login page
+                When: User enters valid email and password and clicks login
+                Then: User is successfully logged in
+            */
+            loginPage.enterYourEmailField().type(this.data.validEmail)
+            loginPage.enterYourPasswordField().type(this.data.validPassword);
+            loginPage.submitLoginButton().click();
+            loginPage.validateLogin();
         });
 
-        it('Test Case 2: Login - Verify if user can Log in with invalid credentials"', () => {
-            loginPage.enterYourEmailField().type(email)
-            loginPage.enterYourPasswordField().type(invalidPassword)
-            loginPage.submitLoginButton().click()
+        it('Test Case 2: Login - Verify if user can Log in with invalid credentials"', function () {
+            /*
+                Given: User is on the login page
+                When: User enters valid email but invalid password and clicks login
+                Then: Authentication failed alert is visible
+            */
+            loginPage.enterYourEmailField().type(this.data.validEmail);
+            loginPage.enterYourPasswordField().type(this.data.invalidPassword);
+            loginPage.submitLoginButton().click();
             loginPage.authenticationFailedAlert().should('be.visible');
         });
 
-        it('Test Case 3: Login - Verify if user can Log in with password field empty"', () => {
-            loginPage.enterYourEmailField().type(invalidEmail)
-            loginPage.submitLoginButton().click()
+        it('Test Case 3: Login - Verify if user can Log in with password field empty"', function () {
+            /*
+                Given: User is on the login page
+                When: User enters an email but leaves the password field empty and clicks login
+                Then: Authentication failed alert is visible
+            */
+            loginPage.enterYourEmailField().type(this.data.invalidEmail);
+            loginPage.submitLoginButton().click();
             loginPage.authenticationFailedAlert().should('be.visible');
         });
 
-        it('Test Case 4: Login - Verify if user can Log in with email field empty"', () => {
-            loginPage.enterYourPasswordField().type(invalidPassword)
-            loginPage.submitLoginButton().click()
+        it('Test Case 4: Login - Verify if user can Log in with email field empty"', function () {
+            /*
+                Given: User is on the login page
+                When: User leaves the email field empty but enters a password and clicks login
+                Then: Authentication failed alert is visible
+            */
+            loginPage.enterYourPasswordField().type(this.data.invalidPassword);
+            loginPage.submitLoginButton().click();
             loginPage.authenticationFailedAlert().should('be.visible');
         });
 
-        it('Test Case 5: Login - Verify if user can Log in all fields empty"', () => {
-            loginPage.submitLoginButton().click()
+        it('Test Case 5: Login - Verify if user can Log in all fields empty"', function () {
+            /*
+                Given: User is on the login page
+                When: User leaves both fields empty and clicks login
+                Then: Authentication failed alert is visible
+            */
+            loginPage.submitLoginButton().click();
             loginPage.authenticationFailedAlert().should('be.visible');
         });
 
-        it('Test Case 6: Login - Verify if user can Log in with invalid email format"', () => {
-            loginPage.enterYourEmailField().type(invalidEmail)
-            loginPage.enterYourPasswordField().type(invalidPassword)
-            loginPage.submitLoginButton().click()
+        it('Test Case 6: Login - Verify if user can Log in with invalid email format"', function () {
+            /*
+                Given: User is on the login page
+                When: User enters an invalid email format and a password and clicks login
+                Then: Authentication failed alert is visible
+            */
+            loginPage.enterYourEmailField().type(this.data.invalidEmail);
+            loginPage.enterYourPasswordField().type(this.data.invalidPassword);
+            loginPage.submitLoginButton().click();
             loginPage.authenticationFailedAlert().should('be.visible');
         });
-
-
     });
 
-    context("Mobile", () => {
-
+    context("Mobile", function () {
         beforeEach(function () {
             cy.viewport(Cypress.env("mobile"));
             cy.visit("/");
         });
 
-        it('Test Case 1: Login - Verify if user can Log in with valid credentials"', () => {
-            loginPage.enterYourEmailField().type(email)
-            loginPage.enterYourPasswordField().type(password)
-            loginPage.submitLoginButton().click()
-            loginPage.validateLogin()
+        it('Test Case 1: Login - Verify if user can Log in with valid credentials"', function () {
+            /*
+                Given: User is on the mobile login page
+                When: User enters valid email and password and clicks login
+                Then: User is successfully logged in
+            */
+            loginPage.enterYourEmailField().type(this.data.validEmail);
+            loginPage.enterYourPasswordField().type(this.data.validPassword);
+            loginPage.submitLoginButton().click();
+            loginPage.validateLogin();
         });
 
-        it('Test Case 2: Login - Verify if user can Log in with invalid credentials"', () => {
-            loginPage.enterYourEmailField().type(invalidEmail)
-            loginPage.enterYourPasswordField().type(invalidPassword)
-            loginPage.submitLoginButton().click()
+        it('Test Case 2: Login - Verify if user can Log in with invalid credentials"', function () {
+            /*
+                Given: User is on the mobile login page
+                When: User enters invalid email and password and clicks login
+                Then: Authentication failed alert is visible
+            */
+            loginPage.enterYourEmailField().type(this.data.invalidEmail);
+            loginPage.enterYourPasswordField().type(this.data.invalidPassword);
+            loginPage.submitLoginButton().click();
             loginPage.authenticationFailedAlert().should('be.visible');
         });
 
-        it('Test Case 3: Login - Verify if user can Log in with password field empty"', () => {
-            loginPage.enterYourEmailField().type(invalidEmail)
-            loginPage.submitLoginButton().click()
+        it('Test Case 3: Login - Verify if user can Log in with password field empty"', function () {
+            /*
+                Given: User is on the mobile login page
+                When: User enters an email but leaves the password field empty and clicks login
+                Then: Authentication failed alert is visible
+            */
+            loginPage.enterYourEmailField().type(this.data.invalidEmail);
+            loginPage.submitLoginButton().click();
             loginPage.authenticationFailedAlert().should('be.visible');
         });
 
-        it('Test Case 4: Login - Verify if user can Log in with email field empty"', () => {
-            loginPage.enterYourPasswordField().type(invalidPassword)
-            loginPage.submitLoginButton().click()
+        it('Test Case 4: Login - Verify if user can Log in with email field empty"', function () {
+            /*
+                Given: User is on the mobile login page
+                When: User leaves the email field empty but enters a password and clicks login
+                Then: Authentication failed alert is visible
+            */
+            loginPage.enterYourPasswordField().type(this.data.invalidPassword);
+            loginPage.submitLoginButton().click();
             loginPage.authenticationFailedAlert().should('be.visible');
         });
 
-        it('Test Case 5: Login - Verify if user can Log in all fields empty"', () => {
-            loginPage.submitLoginButton().click()
-            loginPage.authenticationFailedAlert().should('be.visible');
-        });
-        
-        it('Test Case 6: Login - Verify if user can Log in with invalid email format"', () => {
-            loginPage.enterYourEmailField().type(invalidEmail)
-            loginPage.enterYourPasswordField().type(invalidPassword)
-            loginPage.submitLoginButton().click()
+        it('Test Case 5: Login - Verify if user can Log in all fields empty"', function () {
+            /*
+                Given: User is on the mobile login page
+                When: User leaves both fields empty and clicks login
+                Then: Authentication failed alert is visible
+            */
+            loginPage.submitLoginButton().click();
             loginPage.authenticationFailedAlert().should('be.visible');
         });
 
+        it('Test Case 6: Login - Verify if user can Log in with invalid email format"', function () {
+            /*
+                Given: User is on the mobile login page
+                When: User enters an invalid email format and a password and clicks login
+                Then: Authentication failed alert is visible
+            */
+            loginPage.enterYourEmailField().type(this.data.invalidEmail);
+            loginPage.enterYourPasswordField().type(this.data.invalidPassword);
+            loginPage.submitLoginButton().click();
+            loginPage.authenticationFailedAlert().should('be.visible');
+        });
     });
 });
